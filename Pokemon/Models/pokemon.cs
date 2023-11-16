9275d4df05815cb9167794786d744312e3b7a945
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-11-16 17:49:54 +0100

    Agregar archivos de proyecto.

---
+++ Pokemon/Models/pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace Pokemon.Models
+{
+    public class pokemon
+    {
+        public int numero_pokedex {  get; set; }
+        public string nombre { get; set; }
+        public float peso { get; set; }
+        public float altura { get; set; }
+    }
+}
