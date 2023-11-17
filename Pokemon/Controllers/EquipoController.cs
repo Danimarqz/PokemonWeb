43fe06e6eb6747de216eefd844f385354993d498
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-11-17 18:59:56 +0100

    peso media hecho

--- Pokemon/Controllers/EquipoController.cs
+++ Pokemon/Controllers/EquipoController.cs
@@ -12,12 +12,20 @@ namespace Pokemon.Controllers
         {
             _pokemonRepository = pokemonRepository;
         }
+
         public async Task<IActionResult> Index()
         {
-            var randomPokemons = _pokemonRepository.GetRandom(6);
+            // Espera a que la tarea se complete y obtén el resultado
+            var randomPokemons = await _pokemonRepository.GetRandom(6);
+
+            // Extraer valores de peso de los 6 Pokémon generados
             IEnumerable<float> pesos = randomPokemons.Select(p => p.peso);
-            float media = pesos.Any() ? Statistics.Median(pesos) : 0;
-            ViewBag.MediaPeso = media;
+
+            // Calcular la mediana
+            float mediana = pesos.Any() ? Statistics.Median(pesos) : 0;
+
+            // Pasar la mediana y los 6 Pokémon a la vista
+            ViewBag.MedianaPeso = mediana;
             return View("Index", randomPokemons);
         }
     }
