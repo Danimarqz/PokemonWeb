598bc325b4630af235d2b3dd103a7bf3963aad2d
Danimarqz <daniel.marquez@ciclosmontecastelo.com>
2023-11-18 10:43:41 +0100

    add pesomedio to equipo view

--- Pokemon/Controllers/EquipoController.cs
+++ Pokemon/Controllers/EquipoController.cs
@@ -15,16 +15,10 @@ namespace Pokemon.Controllers
 
         public async Task<IActionResult> Index()
         {
-            // Espera a que la tarea se complete y obtén el resultado
             var randomPokemons = await _pokemonRepository.GetRandom(6);
-
-            // Extraer valores de peso de los 6 Pokémon generados
             IEnumerable<float> pesos = randomPokemons.Select(p => p.peso);
-
-            // Calcular la mediana
             float mediana = pesos.Any() ? Statistics.Median(pesos) : 0;
 
-            // Pasar la mediana y los 6 Pokémon a la vista
             ViewBag.MedianaPeso = mediana;
             return View("Index", randomPokemons);
         }
